e535991b2791c6a1ced87dafb677dafbd2feb821
aacostas25 <aacostas@unal.edu.co>
2022-11-23 13:00:04 -0500

    backend

---
+++ database/migrations/2022_11_23_172422_asignatura.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Asignatura extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('asignatura',function(Blueprint $table){
+            $table ->id();
+            $table -> string('nombre');
+            $table -> integer('creditos');
+            $table -> integer('horas');
+            $table -> string('carrera');
+            $table -> timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
